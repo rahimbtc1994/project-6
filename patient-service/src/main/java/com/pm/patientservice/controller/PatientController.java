b6cb5f5f74daae9e59ffbc1579f4ee9083b3333b
Rahim B. <rahimbtc1994@gmail.com>
2025-04-15 12:51:30 +0100

    patient-service: patient-controller get & test

---
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -0,0 +1,27 @@
+package com.pm.patientservice.controller;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.service.PatientService;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+
+}
